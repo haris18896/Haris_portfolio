52e1005e9d42ae5de144e6b286eefb5614d45946
haris18896 <haris18896@gmail.com>
2024-11-24 23:53:12 +0500

    points has been added

--- sanity/schemaTypes/skills.ts
+++ sanity/schemaTypes/skills.ts
@@ -8,49 +8,34 @@ export const skills = defineType({
   icon: MonitorCogIcon,
   fields: [
     defineField({
-      name: "skill_set",
-      title: "Skills Set",
-      type: "array",
-      of: [
-        {
-          type: "object",
-          fields: [
-            {
-              name: "skill",
-              type: "string",
-              title: "Skill",
-              validation: (Rule) => Rule.required(),
-            },
-            {
-              name: "skill_logo",
-              type: "image",
-              title: "Skill Logo",
-              validation: (Rule) => Rule.required(),
-            },
-            {
-              name: "level",
-              type: "string",
-              title: "Proficiency Level",
-              options: {
-                list: [
-                  "Beginner",
-                  "Intermediate",
-                  "Advanced",
-                  "Expert",
-                  "Master",
-                ],
-              },
-            },
-            {
-              name: "experience",
-              type: "number",
-              title: "Years of Experience",
-            },
-          ],
-        },
-      ],
+      name: "skill",
+      type: "string",
+      title: "Skill",
+      validation: (Rule) => Rule.required().error("Skill name is required"),
+    }),
+    defineField({
+      name: "skill_logo",
+      type: "image",
+      title: "Skill Logo",
+      validation: (Rule) => Rule.required().error("A skill logo is required"),
+    }),
+    defineField({
+      name: "level",
+      type: "string",
+      title: "Proficiency Level",
       validation: (Rule) =>
-        Rule.required().min(1).error("At least one skill is required"),
+        Rule.required()
+          .min(1)
+          .max(10)
+          .error("Points must be a number between 1 and 10"),
+    }),
+
+    defineField({
+      name: "experience",
+      type: "number",
+      title: "Years of Experience",
+      validation: (Rule) =>
+        Rule.min(0).error("Experience must be a positive number"),
     }),
   ],
 });
